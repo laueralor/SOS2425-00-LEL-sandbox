a4bd86aad0865dfceb27f68e8649f51a4734a884
Laura Eraso <laueralor@alum.us.es>
2025-05-09 16:55:11 +0200

    L04

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Init script");
+const express = require("express");
+const cool = require("cool-ascii-faces");
+const app = express();
+const PORT = process.env.PORT || 16078;
+
+app.use("/",express.static("./public"));
+
+app.get("/hello",(request,response)=>{
+    response.send("Hello from the server!");
+});
+
+app.get("/cool",(request,response)=>{
+    response.send(cool());
+});
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}!`);
+});
