d6effa85e2ea88fe819519b3a6a35b5e7b8c41ce
Laura Eraso <laueralor@alum.us.es>
2025-05-09 20:43:27 +0200

    L05

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {loadBackendLEL} from "src\back\home-buying-selling-stats.js";
+import {loadBackendLEL} from "src/back/home-buying-selling-stats.js";
 app.use(express.json);
 
 
