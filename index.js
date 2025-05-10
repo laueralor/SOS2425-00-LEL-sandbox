bd3504d84104d389615a051b481e44153f6f65bc
Laura Eraso <laueralor@alum.us.es>
2025-05-10 10:29:49 +0200

    L06

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 import express from "express";
-import cors from "cors";
 import { loadBackendLEL } from "./src/back/home-buying-selling-stats.js";
 
 
 const app = express();
 const PORT = process.env.PORT || 16078;
 
-const BASE_API = "/api/v1";
 
 app.use(express.json());
-app.use(cors());
+app.use("/", express.static("./about"));
 app.get("/", (_, res) => res.sendStatus(200));   // → devuelve 200 OK
 
 
 loadBackendLEL(app);
 
 app.listen(PORT,()=>{
-    console.log(`Server running port ${PORT}!`);
+    console.log(`Server running on port ${PORT}!`);
 });
